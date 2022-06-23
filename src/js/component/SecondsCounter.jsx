86f05bdc9757fee7e90c23947f4f488ebd9cefb4
camilamasson321 <camilamasson321@gmail.com>
2022-06-23 00:54:35 +0000

    third

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -1,34 +1,26 @@
 import React, { useState, useEffect } from "react";
 
-
 const SecondsCounter = (props) => {
-  console.log(props)
   const [seconds, setSeconds] = useState(0);
-  // useEffect(() => {
-  //   // Update the document title using the browser API
 
-  //   const interval = setInterval(() => {
-  //     setSeconds((seconds) => seconds + 1);
-  //   }, 1000);
-  // });
-
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className="d-flex flex-wrap">
+    <div className="d-flex flex-wrap" >
       <div className="clock">
-      <i className="fas fa-clock"></i>
+        <i className="fas fa-clock"></i>
       </div>
-      <div className="ones">{props.onesecond}</div>
-      <div className="tens">{props.tenseconds}</div>
-      <div className="hundreds">{props.hundredseconds}</div>
       <div className="thousands">{props.thousandsecond}</div>
-
+      <div className="ones">{props.onesecond}</div>
+      <div className="tens">{props.tensecond}</div>
+      <div className="hundreds">{props.hundredsecond}</div>
     </div>
   );
 };
 
 export default SecondsCounter;
-
-// const interval = setInterval(() => {
-//   setSeconds((seconds) => seconds + 1);
-// }, 1000);

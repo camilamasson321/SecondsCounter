af2ffc56c7ce9b217fd6f46696b789546d020929
camilamasson321 <camilamasson321@gmail.com>
2022-06-24 23:28:03 +0000

    final

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const SecondsCounter = (props) => {
+const SecondsCounter = () => {
   const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
@@ -9,16 +9,20 @@ const SecondsCounter = (props) => {
     }, 1000);
     return () => clearInterval(interval);
   }, []);
+  let onesecond = seconds % 10
+  let tensecond= Math.floor(seconds/ 10) % 10
+  let hundredsecond= Math.floor(seconds/100) % 10
+  let thousandsecond=Math.floor(seconds/1000) % 10
 
   return (
-    <div className="d-flex flex-wrap" >
-      <div className="clock">
+    <div className=" main text-white d-flex justify-content-evenly py-2 text-center align-items-center flex-wrap" >
+      <div className="clock card ">
         <i className="fas fa-clock"></i>
       </div>
-      <div className="thousands">{props.thousandsecond}</div>
-      <div className="ones">{props.onesecond}</div>
-      <div className="tens">{props.tensecond}</div>
-      <div className="hundreds">{props.hundredsecond}</div>
+      <div className="card thousands">{thousandsecond}</div>
+      <div className="card hundreds">{hundredsecond}</div>
+      <div className="card tens">{tensecond}</div>
+      <div className="card ones">{onesecond}</div>
     </div>
   );
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-import SecondsCounter from "/workspace/react-hello/src/js/component/SecondsCounter.jsx";
+import SecondsCounter from "/workspace/SecondsCounter/src/js/component/SecondsCounter.jsx";
 
-//import your own components
-
-
-
-//render your react application
-let seconds=0
-
-let onesecond=(seconds/1)
-let tensecond=(seconds/10)
-let hundredsecond=(seconds/100)
-let thousandsecond=(seconds/1000)
-
-
-
-
-ReactDOM.render( <SecondsCounter onesecond={onesecond} tensecond={tensecond} hundredsecond={hundredsecond} thousandsecond={thousandsecond}/>, document.querySelector("#app") )
-
+ReactDOM.render( <SecondsCounter />, document.querySelector("#app") )
